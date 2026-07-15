e408e2aaa5096e36ac81c28f774b167421c58511
zaid-mian <mianzaid049@gmail.com>
2026-07-15 17:59:02 +0500

    Refactor: Deletion of crm app and final cleanup complete. Catalog app fully active

--- crm/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .product import Product
-from .feature import Feature
-from .plan import PricingPlan, PlanFeature
-
-__all__ = ['Product', 'Feature', 'PricingPlan', 'PlanFeature']
--- crm/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .product import CRMProductAPIListView, CRMProductAPIDetailView
